fada585455602490651f0959095c6f56fbc14d53
Natalia-dot <nataliadurantegarcia@gmail.com>
2024-05-22 18:48:28 +0200

    icosahedron scene

--- 2.-Advanced-scene/src/App.jsx
+++ 2.-Advanced-scene/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Canvas, useThree, useFrame } from '@react-three/fiber'
+import { Vector3 } from 'three'
+import { Stats, Environment, Center } from '@react-three/drei'
+import Button from './Button'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const vec = new Vector3()
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Rig() {
+  return useFrame(({ camera, mouse }) => {
+    // vec.set(mouse.x * 2, 3 + (mouse.y * 2), camera.position.z) this would be a way to have the camera start from above, by
+    //adding a fixed "base" value to the camera position   
+    vec.set(mouse.x * 2, mouse.y * 2, camera.position.z)
+    camera.position.lerp(vec, 0.025)
+    camera.lookAt(0, 0, 0)
+  })
 }
 
-export default App
+export default function App() {
+  return (
+    <Canvas camera={{ position: [0, 0, 5] }}>
+      <Environment preset="city" background />
+      <Center>
+        {[...Array(5).keys()].map((x) =>
+          [...Array(3).keys()].map((y) => (
+            <Button key={x + y * 5} position={[x * 2.5, y * 2.5, 0]} />
+          ))
+        )}
+      </Center>
+      <Rig />
+      {//stats is the little square appearing on the top left corner showing fps and such
+      }
+      <Stats />
+    </Canvas>
+  )
+}
---
+++ 2.-Advanced-scene/src/Button.jsx
@@ -0,0 +1,50 @@
+
+import { useRef, useState, useMemo } from 'react'
+import { useFrame } from '@react-three/fiber'
+import { MathUtils } from 'three'
+import { Color } from 'three'
+import { randomNumber } from './utils/randomNumber'
+
+const black = new Color('black')
+export default function Button(props) {
+  const ref = useRef()
+  const [hovered, setHovered] = useState(false)
+  const [selected, setSelected] = useState(false)
+  const colorTo = useMemo(
+    () => new Color(Math.floor(Math.random() * 16777216)), 
+    //this is a hex triplet form of color. Basically it is a hexadecimal number (the kind that CAN have letters in it),
+    //and each pair of elements refers to the red green blue and alpha channels respectively.
+    []
+  )
+  useFrame(() => {
+    ref.current.rotation.x = hovered
+      ? MathUtils.lerp(ref.current.rotation.x, -Math.PI * 2, 0.025)
+      : MathUtils.lerp(ref.current.rotation.x, 0, 0.025)
+
+    ref.current.position.z = selected
+      ? MathUtils.lerp(ref.current.position.z, 0, 0.025)
+      : MathUtils.lerp(ref.current.position.z, -3, 0.025)
+
+    ref.current.material.color.lerp(selected ? colorTo : black, 0.025)
+  })
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      onPointerDown={() => {
+        setSelected(!selected)
+      }}
+      onPointerOver={() => setHovered(true)}
+      onPointerOut={() => setHovered(false)}
+    >
+      <icosahedronGeometry />
+      <meshPhysicalMaterial
+        roughness={0}
+        metalness={0}
+        thickness={3.12}
+        ior={1.74}
+        transmission={1.0}
+      />
+    </mesh>
+  )
+}
