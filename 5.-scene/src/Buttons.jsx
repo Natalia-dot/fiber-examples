b621f0fd539332178f226abe0070699288a4d031
Natalia-dot <nataliadurantegarcia@gmail.com>
2024-06-14 09:05:39 +0200

    Media querying and finishing touches

--- 5.-scene/src/Buttons.jsx
+++ 5.-scene/src/Buttons.jsx
@@ -1,22 +1,29 @@
+import { useState } from 'react'
 import { useCamContext } from './context'
 
 export const Buttons = (props) => {
   let { camPosition, setCamPosition } = useCamContext()
+
+  const [activeCam, setActiveCam] = useState(1)
   const handleCamera = (type) => {
     switch (type) {
       case 1:
         setCamPosition([0, 3, -4])
+        setActiveCam(type)
         break
       case 2:
         setCamPosition([8, 1, 0])
+        setActiveCam(type)
 
         break
       case 3:
         setCamPosition([13, 15, -4])
+        setActiveCam(type)
 
         break
       case 4:
         setCamPosition([-13, 15, 8])
+        setActiveCam(type)
 
         break
 
@@ -30,13 +37,19 @@ export const Buttons = (props) => {
         <button className="camButton" onClick={() => handleCamera(1)}>
           Reset
         </button>
-        <button className="camButton" onClick={() => handleCamera(2)}>
+        <button
+          className={`camButton ${activeCam == 2 ? 'activeCamButton' : ''}`}
+          onClick={() => handleCamera(2)}>
           Cam 1
         </button>
-        <button className="camButton" onClick={() => handleCamera(3)}>
+        <button
+          className={`camButton ${activeCam == 3 ? 'activeCamButton' : ''}`}
+          onClick={() => handleCamera(3)}>
           Cam 2
         </button>
-        <button className="camButton" onClick={() => handleCamera(4)}>
+        <button
+          className={`camButton ${activeCam == 4 ? 'activeCamButton' : ''}`}
+          onClick={() => handleCamera(4)}>
           Cam 3
         </button>
       </div>
