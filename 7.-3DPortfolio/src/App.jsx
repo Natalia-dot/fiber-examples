31076446a10a1958220eed17cdb39a79b72d5728
Natalia-dot <nataliadurantegarcia@gmail.com>
2024-07-01 21:19:46 +0200

    Environment settings and other misc in app

--- 7.-3DPortfolio/src/App.jsx
+++ 7.-3DPortfolio/src/App.jsx
@@ -1,18 +1,31 @@
-import React, { Suspense, useState } from 'react'
-import { Canvas, useFrame, useLoader } from '@react-three/fiber'
-import { Loader, OrbitControls, useGLTF } from '@react-three/drei'
-
-import { useControls } from 'leva'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import {
+  CameraControls,
+  Environment,
+  Loader,
+  OrbitControls
+} from '@react-three/drei'
 import { Outlet, useNavigate } from 'react-router-dom'
+import { AxesHelper, Euler, Vector3 } from 'three'
+import { degToRad } from 'three/src/math/MathUtils.js'
+import { useControls } from 'leva'
 
 export const App = () => {
   const navigate = useNavigate()
   return (
     <>
       <Suspense fallback={null} />
-      <Canvas camera={{ position: [0, 0, -2] }}>
-        {/* <OrbitControls /> */}
+      <Canvas
+        camera={{
+          position: [0, 0, -1],
+          rotateX: degToRad(45)
+        }}>
+        <axesHelper size={10} />
+
+        <Environment preset="apartment" background />
         <directionalLight position={[-5, -1, -10]} intensity={1} />
+        <CameraControls smoothTime={1.5} />
         <Outlet />
       </Canvas>
       <div id="overlay">
