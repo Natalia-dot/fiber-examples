cd2519ce510bb024258347485558bc48c78994ca
Natalia-dot <nataliadurantegarcia@gmail.com>
2024-07-01 21:20:13 +0200

    Full Sword small edits

--- 7.-3DPortfolio/src/FullSword.jsx
+++ 7.-3DPortfolio/src/FullSword.jsx
@@ -1,12 +1,13 @@
-import { useFrame, useLoader } from '@react-three/fiber'
-import { useRef, useState } from 'react'
+import { useFrame, useLoader, useThree } from '@react-three/fiber'
+import { useEffect, useRef, useState } from 'react'
 import { MathUtils } from 'three'
 import { GLTFLoader } from 'three/examples/jsm/Addons.js'
 
 export const FullSword = () => {
   const ref = useRef()
-
+  const three = useThree()
   const gltf = useLoader(GLTFLoader, '../BusterSwordModeladoEnd.glb')
+  console.log(gltf)
   const [isHovered, setIsHovered] = useState(false)
 
   useFrame(() => {
@@ -14,21 +15,19 @@ export const FullSword = () => {
       ? MathUtils.lerp(ref.current.position.z, -1, 0.1)
       : MathUtils.lerp(ref.current.position.z, 0, 0.1)
 
-    ref.current.rotation.y = isHovered
-      ? MathUtils.lerp(ref.current.rotation.y, 0.2, 0.1)
-      : MathUtils.lerp(ref.current.rotation.y, 0, 0.1)
+    // ref.current.rotation.y = isHovered
+    //   ? MathUtils.lerp(ref.current.rotation.y, 0.2, 0.1)
+    //   : MathUtils.lerp(ref.current.rotation.y, 0, 0.1)
 
-    ref.current.position.y = isHovered
-      ? MathUtils.lerp(ref.current.position.y, 1, 0.1)
-      : MathUtils.lerp(ref.current.position.y, 0.5, 0.1)
+    // ref.current.position.y = isHovered
+    //   ? MathUtils.lerp(ref.current.position.y, 1, 0.1)
+    //   : MathUtils.lerp(ref.current.position.y, 0, 0.1)
   })
 
   return (
     <primitive
       object={gltf.scene}
-      position={[2.2, 0.5, 0]}
-      rotation={[1.5, 0, 0]}
-      scale={0.1}
+      position={[0, 0, 0]}
       ref={ref}
       onPointerOver={(e) => setIsHovered(true)}
       onPointerOut={(e) => setIsHovered(false)}
